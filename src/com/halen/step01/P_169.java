8970b43c4673de8e83223ae2026158f4a90ec9cc
Halen <911452497@qq.com>
2019-03-28 16:34:44 +0800

    求众数

--- src/com/halen/step01/P_169.java
+++ src/com/halen/step01/P_169.java
@@ -31,5 +31,20 @@ public class P_169 {
       return res;
     }
 	
+	public int majorityElement_2(int[] nums) {
+        int count = 1;
+        int res = nums[0];
+        for(int i=1;i<nums.length;++i){
+            if(res == nums[i])
+                ++count;
+            else{
+                --count;
+                if(count==0){
+                    res = nums[i+1];
+                }
+            }
+        }
+        return res;
+    }
 	
 }
