616a054a3c1563684f9760fe7d88649071a61328
Halen <911452497@qq.com>
2019-04-29 18:17:06 +0800

    链表的中间结点

--- src/com/halen/step01/P_876.java
+++ src/com/halen/step01/P_876.java
@@ -1,5 +1,34 @@
 package com.halen.step01;
 
+/**
+ * 链表的中间结点，LeetCode第876题
+ * @author halen
+ *
+ */
 public class P_876 {
-
+	//使用快慢指针
+	public ListNode middleNode(ListNode head) {
+        if(head.next==null)
+        	return head;
+        ListNode fastNode = head;
+        ListNode lowNode = head;
+        while(fastNode!=null && fastNode.next!=null) {
+        	fastNode=fastNode.next.next;
+        	lowNode = lowNode.next;
+        }
+        return lowNode;
+    }
+	
+	//按顺序将每个节点放入数组，中间结点就是A[A.length/2]
+	public ListNode middleNode_2(ListNode head) {
+        ListNode[] A = new ListNode[100];
+        int t = 0;
+        while(head!=null) {
+        	A[t++] = head;
+        	head = head.next;
+        }
+        return A[t/2];
+    }
+	
+	
 }
