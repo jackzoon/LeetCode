68fb8c64e64298912c7d7df647c48d2cd5df3497
Halen <911452497@qq.com>
2019-05-20 13:39:20 +0800

    分糖果

--- src/com/halen/step01/P_575.java
+++ src/com/halen/step01/P_575.java
@@ -3,6 +3,11 @@ package com.halen.step01;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * 分糖果，LeetCode第575题
+ * @author halen
+ *
+ */
 public class P_575 {
 	public int distributeCandies(int[] candies) {
         
